62fcbd316cf4d4b9ab4c2d3507f55992d815fc0d
Reagan Ibbotson <reagan_ib@hotmail.com>
2018-09-09 21:31:33 +1000

    Added review endpoints

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+
+
 app.post('/signup',(req, res)=>{
 	const { email, fullName, password } = req.body;
 	const hash = bcrypt.hashSync(password, 10);
@@ -52,8 +54,40 @@ app.post('/login', (req, res)=>{
 	.catch(err=> res.status(400).json('Invalid credentials'))
 })
 
+app.put('/leave-review', (req, res)=>{
+	const { userID, resortID, overallRating, powderRating, crowdRating, villageRating, priceRating } = req.body;
+	db.insert({
+		user_id: userID,
+		resort_id: resortID,
+		total_score: overallRating,
+		powder_score: powderRating,
+		crowd_score: crowdRating,
+		village_score: villageRating,
+		price_score:  priceRating
+	}).into('reviews')
+	.then(res.status(200).json('Review submitted'))
+	.catch(res.status(400).json('Error submitting review'))
+})
+
+app.post('/see-review', (req, res)=>{
+	const { resortID } = req.body;
+	db.select(db.raw(`
+		resort_id, 
+		avg(total_score) as total_score, 
+		avg(powder_score) as powder_score, 
+		avg(crowd_score) as crowd_score, 
+		avg(village_score) as village_score, 
+		avg(price_score) as price_score`))
+	.from('reviews')
+	.where('resort_id', '=', resortID)
+	.groupBy('resort_id')
+	.returning('resort_id')
+	.then(data=>{
+		res.status(200).json(data[0]);
+	})
+	.catch(err=> res.status(400).json('Unable to retrieve reviews'));
+})
+
 app.listen(3000, ()=>{
 	console.log("shitfuckball");
-	db.select('*').from('users')
-		.then(data=> console.log)
 });
