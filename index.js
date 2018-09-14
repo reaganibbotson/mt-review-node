44db33b20582370d8a2ec4d312373da3e0f99e50
Reagan Ibbotson <ReaganI@pvsoz.com.au>
2018-09-14 15:26:31 +1000

    Fixes

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+
 app.get('/regions', (req, res)=>{
 	db.select(db.raw('distinct region'))
 	.from('resorts')
@@ -48,12 +49,12 @@ app.get('/resorts/:region', (req, res)=>{
 	.catch(err=>res.status(400).json('Unable to retrieve resort list' + err));
 })
 
-app.post('/resort/:resort_name', (req, res)=>{
-	const { resort_name } = req.body;
+app.get('/resort/:resort_name', (req, res)=>{
+	const { resort_name } = req.params;
 	db.select('*')
 	.from('resorts')
 	.where('resort_name','=', resort_name)
-	.then(data=>res.status(200).json(data[0]))
+	.then(data=>res.status(200).json(data))
 	.catch(err=>res.status(400).json(`Unable to retrieve resort info. ${err}`))
 })
 
